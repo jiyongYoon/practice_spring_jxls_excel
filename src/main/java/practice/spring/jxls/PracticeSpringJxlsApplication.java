267cf3e7c42a6cf257d8aa036c6dfcd8de0c01e7
jiyong.yoon <jyyoon0615@naver.com>
2025-01-10 17:54:05 +0900

    git init

---
+++ src/main/java/practice/spring/jxls/PracticeSpringJxlsApplication.java
@@ -0,0 +1,13 @@
+package practice.spring.jxls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeSpringJxlsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PracticeSpringJxlsApplication.class, args);
+  }
+
+}
---
+++ src/test/java/practice/spring/jxls/PracticeSpringJxlsApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.spring.jxls;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeSpringJxlsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
